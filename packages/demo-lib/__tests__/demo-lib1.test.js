d33f775913d6ec9eeb92015acc453c36f4447f39
Chris Lu Yi <chris_lu@medinno.onmicrosoft.com>
2020-11-25 19:58:00 +0800

    1

--- packages/demo-lib/__tests__/demo-lib1.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const demoLib1 = require("../lib/demo-lib2");
-
-describe("demo-lib2", () => {
-  console.log(demoLib1);
-  it("needs tests");
-});
--- packages/demo-lib13/src/index.js
+++
@@ -1,6 +0,0 @@
-import fn from "../../demo-lib2/src/node_modules/@chrisluyi/demo-lib2";
-
-export function test() {
-  console.log(fn);
-  return 12313;
-}
